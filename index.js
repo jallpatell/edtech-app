698c83830d9d2debf0fe5940626f98740212d7b2
Jal <11jal.edu@gmail.com>
2025-01-08 02:14:18 +0530

    skeleton

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const jwt = require('jsonwebtoken')
+const key = require('./jwt')
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.json({
+        "message":"you are at homepage yada yada !"
+    })
+})
+
+app.get('/signin', (req, res) => {
+    const username = req.body.username
+    const password = req.body.password
+
+    res.json({
+        "message": "you are signed in"
+    })
+})
+app.get('/signup', (req, res) => {
+
+})
+
+app.listen(3000, () => { console.log('start')})
