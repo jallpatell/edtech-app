31969a99481e13d2f9d53cc9205ea5808dae9f33
Jal <11jal.edu@gmail.com>
2025-01-08 02:15:41 +0530

    login and sigin endpoints

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const jwt = require('jsonwebtoken')
-const key = require('./jwt')
+const key = require('./jwtn')
 
 app.use(express.json())
 
